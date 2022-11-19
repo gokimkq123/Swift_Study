a7e0e1f91773832108f18de3279e670a2a0dfb7a
kim geon han <gokimkq111@naver.com>
2022-11-20 00:37:57 +0900

    Current-time and Picker-time

---
+++ DatePicker.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Swift_Code
+//
+//  Created by geonhan on 2022/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblSelectTime: UILabel!
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+        
+    }
+
+   
+    @IBAction func changeDateSelect(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        
+        lblSelectTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+    }
+    
+    @objc func updateTime(){
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+    }
+}
+
